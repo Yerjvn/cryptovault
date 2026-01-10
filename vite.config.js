1bf7af2144f972130be15070c2cdb149c563990b
nurzhan.m <nurzhan@snoonu.com>
2026-01-10 13:20:50 +0500

    Rename to lowercase cryptovault

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CryptoVault/',  // Имя репозитория для GitHub Pages
+  base: '/cryptovault/',  // Имя репозитория для GitHub Pages
   server: {
     port: 3000,
     open: true
