d81185c2f994f74c6fe87bc195060beee122cb88
nurzhan.m <nurzhan@snoonu.com>
2026-01-10 13:16:02 +0500

    Rename project to CryptoVault

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wallet/',  // Имя репозитория для GitHub Pages
+  base: '/CryptoVault/',  // Имя репозитория для GitHub Pages
   server: {
     port: 3000,
     open: true
